cc1d713def94d90aba6d5ec8dbce6c8a63303fcc
Nilam20 <neelamkvasu20@gmail.com>
2023-06-30 17:58:20 +0530

    space added

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace ContactsAPI.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateTime.Now.AddDays(index), 
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
